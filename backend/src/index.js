43b914f6fe127eccecd1caaef41b8515655b6543
maxpilipovic <maxpkusa@gmail.com>
2025-09-30 22:25:38 -0500

    Made some changes to backend routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import e from 'express';
 
 
 
@@ -76,3 +77,143 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`API available at http://localhost:${PORT}`);
 });
+
+//Post routes for entering data...
+
+//ROLES TABLE
+app.post('/api/roles', async (req, res) => {
+
+  const { rolename, permission_level} = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('roles')
+      .insert([{ rolename, permission_level }])
+      .select();
+
+    if (error) {
+      return res.status(500).json({ error: 'Failed to add role data' });
+    }
+
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting role data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//USERS TABLE
+app.post('/api/users', async (req, res) => {
+  
+  const { first_name, last_name, role_id, pin_code, is_active,  } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('users')
+      .insert([{ first_name, last_name, role_id, pin_code, is_active }])
+      .select();
+
+    if (error) {
+      return res.status(500).json({ error: 'Failed to add user data' });
+    }
+
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting user data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//TABLES TABLE
+app.post('/api/tables', async (req, res) => {
+
+  const { table_number, status, assigned_waiter_id } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('tables')
+      .insert([{ table_number, status, assigned_waiter_id }])
+      .select();
+
+    if (error) {
+      console.error("Supabase insert error:", error);
+      return res.status(500).json({
+        error: 'Failed to add table data',
+        details: error
+      });
+    }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting table data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//MENU TABLE
+app.post('/api/menu', async (req, res) => {
+
+  const { name, description, price, category, is_available } = req.body;
+
+  try {
+    const {data, error } = await supabase
+      .from('menu_items')
+      .insert([{ name, description, price, category, is_available }])
+      .select();
+      
+    if (error) {
+      return res.status(500).json({ error: 'Failed to add menu item data' });
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting menu item data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//ORDERS TABLE
+app.post('/api/orders', async (req, res) => {
+
+  const {table_id, waiter_id, order_status, total_amount, tip_amount } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('orders')
+      .insert([{ table_id, waiter_id, order_status, total_amount, tip_amount }])
+      .select();
+
+      if (error) {
+      return res.status(500).json({ error: 'Failed to add order data' });
+    }
+
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting order data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+//ORDER_ITEMS TABLE
+app.post('/api/order_items', async (req, res) => {
+
+  const { order_id, item_id, quantity, special_requests } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('order_items')
+      .insert([{ order_id, item_id, quantity, special_requests }])
+      .select();
+
+    if (error) {
+      return res.status(500).json({ error: 'Failed to add order_item data' });
+    }
+
+  } catch (error) {
+    console.error(error);
+    console.log("Error inserting order_item data");
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
