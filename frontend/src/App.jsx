a3f3b2dd6e063ab06b0838c8dbe6d482f66715ba
MaxPilipovic <maxpkusa@gmail.com>
2025-11-03 12:45:00 -0600

    Fixed a shit ton of bugs across platform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,12 +20,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const location = useLocation();
-  const showNavbar = location.pathname !== '/'; //Temp fix to hide navbar on login page
+  const showNavbar = location.pathname !== '/'; //Does not display navbar on login page;
+  const showNavbar2 = location.pathname !== '/create-account'; //Does not display navbar on create account page;
 
   return (
     <>
      <AuthProvider>
-      {showNavbar && <Navbar />}
+      {showNavbar && showNavbar2 && <Navbar />}
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/create-account" element={<CreateAccount />} />
@@ -36,7 +37,7 @@ function App() {
         <Route path="/waiter" element={<PageWaiter />} />
         <Route path="/manager" element={<PageManager />} />
         <Route path="/owner" element={<PageOwner />} />
-        <Route path="/create-order" element={<CreateOrder />} /> 
+        <Route path="/create-order/:tableId" element={<CreateOrder />} /> 
       </Routes>
     </AuthProvider>
       {/* import toast container */}
